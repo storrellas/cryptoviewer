43b2eb6fc310b723ff227f05206f05022f3a9ab2
storrella <storrellas@gmail.com>
2018-10-19 00:06:42 +0200

    ENH: Added template for admin

--- viewer/urls.py
+++ viewer/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 # Set URL patterns
 urlpatterns = []
+urlpatterns += url(r'^tables/', views.TableView.as_view()),
 urlpatterns += url('', views.IndexView.as_view()),
--- viewer/views.py
+++ viewer/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+class TableView(TemplateView):
+    template_name = "tables.html"
