b6bc89f3257d2eddcaa23d88d2bc548aa6e9a550
storrellas <storrellas@gmail.com>
2018-10-19 13:35:48 +0000

    ENH: Generating new templates

--- viewer/urls.py
+++ viewer/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 # Set URL patterns
 urlpatterns = []
-urlpatterns += url(r'^tables/', views.TableView.as_view()),
+urlpatterns += url(r'^instrument/', views.TableInstrumentView.as_view()),
+urlpatterns += url(r'^trade/', views.TableTradeView.as_view()),
 urlpatterns += url('', views.IndexView.as_view()),
--- viewer/views.py
+++ viewer/views.py
@@ -8,7 +8,7 @@ from django.conf import settings
 from deribit_api import RestClient
 
 # Project imports
-from .models import Instrument
+from .models import Instrument, Trade
 
 class IndexView(TemplateView):
     template_name = "index.html"
@@ -17,26 +17,61 @@ class IndexView(TemplateView):
 # class TableView(TemplateView):
 #     template_name = "tables.html"
 
-class TableView(View):
+class TableInstrumentView(View):
     def get(self, request):
-
-        print(settings.DERIBIT_KEY)
-        print(settings.DERIBIT_SECRET)
 
         # Generate Deribit client
         client = RestClient(settings.DERIBIT_KEY, settings.DERIBIT_SECRET)
         # Get instruments
-        instruments = client.getinstruments()
+        instrument_list = client.getinstruments()
 
         # Create instruments
         # TODO: Make use of bulk_create here
-        for instrument in instruments:
+        for instrument in instrument_list:
             try:
                 instruments_model = Instrument(**instrument)
                 instruments_model.save()
-            except:
+
+            except Exception as e:
                 # TODO: Integrate logger here
                 print("Error while saving model")
 
-        context = {'instument_list': Instrument.objects.all()}
-        return render(request, 'tables.html', context)
+
+
+        context = {'instrument_list': Instrument.objects.all()}
+        return render(request, 'table_instrument.html', context)
+
+class TableTradeView(View):
+    def get(self, request):
+
+        # Generate Deribit client
+        client = RestClient(settings.DERIBIT_KEY, settings.DERIBIT_SECRET)
+
+        print("Getting this thing")
+        print(Instrument.objects.first().instrumentName)
+
+        # Get instruments
+        instruments = client.getinstruments()
+        trade_list = client.getlasttrades(instruments[1]['instrumentName'])
+
+        instruments_model = Instrument(**instruments[1])
+        instruments_model.save()
+
+        #trade_list = client.getlasttrades(Instrument.objects.get(id=827).instrumentName)
+
+        print(trade_list)
+
+        # Create instruments
+        # TODO: Make use of bulk_create here
+        for trade in trade_list:
+            try:
+                trade_model = Trade(**trade)
+                trade_model.instrument = instrument_model
+                trade_model.save()
+            except Exception as e:
+                # TODO: Integrate logger here
+                print("Error while saving model")
+                print(e)
+
+        context = {'trade_list': Trade.objects.all()}
+        return render(request, 'table_trade.html', context)
