09d514a02563207461dbfdfa77fe6a1a540831c9
storrella <storrellas@gmail.com>
2018-10-21 00:53:26 +0200

    ENH: Deleted migrations

--- viewer/management/commands/retrieval.py
+++ viewer/management/commands/retrieval.py
@@ -1,6 +1,7 @@
 # Python imports
 import os
 import time
+import traceback
 from deribit_api import RestClient
 
 # Django imports
@@ -30,16 +31,38 @@ class Command(BaseCommand):
         try:
             logger.info("Deleting all Instrument objects")
             Instrument.objects.all().delete()
+            Trade.objects.all().delete()
+
+            # Iterate instruments
             instrument_model_list = []
             for instrument in instrument_list:
                 instrument_model = Instrument(**instrument)
                 instrument_model_list.append(instrument_model)
-
-
-
-            Instrument.objects.bulk_create(instrument_model_list)
+            if len(instrument_model_list) > 0:
+                Instrument.objects.bulk_create(instrument_model_list)
             logger.info("Inserted " + str(len(instrument_model_list)) + " Instrument objects")
+
+
+            # Get Last trades
+            for instrument in Instrument.objects.all():
+
+                # Get Last Trades
+                trade_model_list = []
+                trade_list = client.getlasttrades(instrument.instrumentName)
+                for trade in trade_list:
+                    del trade['instrument']
+                    trade_model = Trade(**trade)
+                    trade_model.instrument = instrument
+                    #trade_model.save()
+                    trade_model_list.append(trade_model)
+                if len(trade_model_list) > 0:
+                    Trade.objects.bulk_create(trade_model_list)
+                logger.info("Inserted " + str(len(trade_model_list)) +
+                                " Trade objects for InstrumentName " + instrument.instrumentName)
+
+            logger.info("Inserted " + str(Trade.objects.all().count()) + " Trades" )
         except Exception as e:
+            traceback.print_exc()
             logger.error(e)
 
         logger.info('-- Ending process --')
