33578d3bea3454247673fb06505ee1ca7a6e04c0
storrellas <storrellas@gmail.com>
2018-10-22 09:47:02 +0000

    ENH: Adding configuration

--- viewer/models.py
+++ viewer/models.py
@@ -6,23 +6,23 @@ from django.db import models
 # 'baseCurrency': 'BTC', 'expiration': '2019-06-28 08:00:00 GMT', 'tickSize': 0.0005,
 # 'instrumentName': 'BTC-28JUN19-20000-C', 'created': '2018-10-04 11:14:09 GMT'}
 class Instrument(models.Model):
-    kind = models.CharField(max_length=10,null=True)
-    currency = models.CharField(max_length=10,null=True)
+    kind = models.CharField(max_length=500,null=True)
+    currency = models.CharField(max_length=500,null=True)
     pricePrecision = models.IntegerField(null=True)
     strike = models.DecimalField(max_digits=10, decimal_places=5,null=True)
     optionType = models.CharField(max_length=10,null=True)
     isActive = models.BooleanField(null=True)
     minTradeSize = models.DecimalField(max_digits=10, decimal_places=5,null=True)
-    settlement = models.CharField(max_length=10,null=True)
-    baseCurrency = models.CharField(max_length=10,null=True)
+    settlement = models.CharField(max_length=500,null=True)
+    baseCurrency = models.CharField(max_length=500,null=True)
     # TODO: Find a way to import DateTimeField as it comes from Deribit
     #expiration = models.DateTimeField(null=True)
-    expiration = models.CharField(max_length=20,null=True)
+    expiration = models.CharField(max_length=500,null=True)
     tickSize = models.DecimalField(max_digits=10, decimal_places=5,null=True)
-    instrumentName = models.CharField(max_length=100,null=True)
+    instrumentName = models.CharField(max_length=500,null=True)
     # TODO: Find a way to import DateTimeField as it comes from Deribit
     #created = models.DateTimeField(null=True)
-    created = models.CharField(max_length=20,null=True)
+    created = models.CharField(max_length=500,null=True)
     contractSize = models.DecimalField(max_digits=10, decimal_places=5,null=True)
 
 
@@ -36,7 +36,7 @@ class Trade(models.Model):
     quantity = models.DecimalField(max_digits=10, decimal_places=5,null=True)
     tickDirection = models.IntegerField(null=True)
     tradeSeq = models.IntegerField(null=True)
-    direction = models.CharField(max_length=10,null=True)
+    direction = models.CharField(max_length=500,null=True)
     amount = models.DecimalField(max_digits=10, decimal_places=5,null=True)
     timeStamp = models.IntegerField(null=True)
     price = models.DecimalField(max_digits=10, decimal_places=5,null=True)
