4cf495c4d180ae2b4e510a8cd1b845a76802f043
storrellas <storrellas@gmail.com>
2018-10-22 09:21:08 +0000

    ENH: Removed django_heroku

--- cryptoviewer/settings.py
+++ cryptoviewer/settings.py
@@ -86,6 +86,7 @@ import dj_database_url
 DATABASES = {}
 #DATABASES['default'] = dj_database_url.config(default='postgres://iwhdhaahrarrpx:cd6b69b40ec87ad598271399d6dea0bb3904f8207c59c38d455a1757f97508bc@ec2-54-243-147-162.compute-1.amazonaws.com:5432/dfcf4ac70kv6tf')
 DATABASES['default'] = dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
